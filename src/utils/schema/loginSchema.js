be8dccd0eba6e58617b782b0acdf346efc71d1db
irwanadinata <irwanadinata9@gmail.com>
2023-12-22 02:40:10 +0700

    chore: feature login

---
+++ src/utils/schema/loginSchema.js
@@ -0,0 +1,6 @@
+import { z } from 'zod';
+
+export const loginSchema = z.object({
+  email: z.string().email({ message: 'Email tidak valid' }),
+  password: z.string().min(6, { message: 'Password minimal 6 karakter' }),
+});
