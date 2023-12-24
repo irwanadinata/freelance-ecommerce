2489d375f08641505ef964f126049d940a8e46a6
irwanadinata <irwanadinata9@gmail.com>
2023-12-25 02:59:06 +0700

    chore: create menu component

---
+++ src/components/menu.jsx
@@ -0,0 +1,26 @@
+import MenuLazada from "@/assets/menu-lazada.png";
+import TopUp from "@/assets/top-up.png";
+import Voucher from "@/assets/voucher.png";
+
+const Menu = () => {
+  return (
+    <>
+      <div class="flex shadow-lg my-4 lg:w-10/12 md:w-full mx-auto rounded-md bg-[#FFFFFF]">
+        <div class="w-1/3 flex items-center justify-center">
+          <img src={MenuLazada} alt="menu-lazada-image" class="mr-1" />
+          <a href=""><p className="font-semibold">LazMall</p></a>
+        </div>
+        <div class="w-1/3 flex items-center justify-center">
+          <img src={TopUp} alt="top-up-image" class="mr-1" />
+          <a href=""><p className="font-semibold">TopUp</p></a>
+        </div>
+        <div class="w-1/3 flex items-center justify-center">
+          <img src={Voucher} alt="voucher-image" class="mr-1" />
+          <a href=""><p className="font-semibold ml-1">Voucher</p></a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Menu;
