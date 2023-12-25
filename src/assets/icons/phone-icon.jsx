3790361966d70038eed9b171b3fd3ff61b060ca9
Sam <samilalghiffary@gmail.com>
2023-12-25 22:46:39 +0700

    Add icons for detail product

---
+++ src/assets/icons/phone-icon.jsx
@@ -0,0 +1,14 @@
+function PhoneIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="none" viewBox="0 0 30 30">
+      <path
+        fill="#000"
+        fillRule="evenodd"
+        d="M8.25.75A2.25 2.25 0 006 3v24a2.25 2.25 0 002.25 2.25h13.5A2.25 2.25 0 0024 27V3A2.25 2.25 0 0021.75.75H8.25zM7.5 3a.75.75 0 01.75-.75h2.25v1.5a.75.75 0 00.75.75h7.5a.75.75 0 00.75-.75v-1.5h2.25a.75.75 0 01.75.75v24a.75.75 0 01-.75.75H8.25A.75.75 0 017.5 27V3z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  );
+}
+
+export default PhoneIcon;
