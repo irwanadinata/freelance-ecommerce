3836aa6c6b38ff80840ef8f47b239850331f91f0
Sam <samilalghiffary@gmail.com>
2023-12-29 07:49:39 +0700

    fix: Changes class into className

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -5,18 +5,24 @@ import Voucher from "@/assets/voucher.png";
 const Menu = () => {
   return (
     <>
-      <div class="flex shadow-md my-2 lg:w-10/12 md:w-full mx-auto rounded-md bg-[#FFFFFF]">
-        <div class="w-1/3 flex items-center justify-center">
-          <img src={MenuLazada} alt="menu-lazada-image" class="mr-1" />
-          <a href=""><p className="font-semibold">LazMall</p></a>
+      <div className="flex shadow-md my-2 lg:w-10/12 md:w-full mx-auto rounded-md bg-[#FFFFFF]">
+        <div className="w-1/3 flex items-center justify-center">
+          <img src={MenuLazada} alt="menu-lazada-image" className="mr-1" />
+          <a href="">
+            <p className="font-semibold">LazMall</p>
+          </a>
         </div>
-        <div class="w-1/3 flex items-center justify-center">
-          <img src={TopUp} alt="top-up-image" class="mr-1" />
-          <a href=""><p className="font-semibold">TopUp</p></a>
+        <div className="w-1/3 flex items-center justify-center">
+          <img src={TopUp} alt="top-up-image" className="mr-1" />
+          <a href="">
+            <p className="font-semibold">TopUp</p>
+          </a>
         </div>
-        <div class="w-1/3 flex items-center justify-center">
-          <img src={Voucher} alt="voucher-image" class="mr-1" />
-          <a href=""><p className="font-semibold ml-1">Voucher</p></a>
+        <div className="w-1/3 flex items-center justify-center">
+          <img src={Voucher} alt="voucher-image" className="mr-1" />
+          <a href="">
+            <p className="font-semibold ml-1">Voucher</p>
+          </a>
         </div>
       </div>
     </>
