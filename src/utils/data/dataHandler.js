a986560302d1a059343121af49696913bc97afea
Sam <samilalghiffary@gmail.com>
2023-12-29 20:08:01 +0700

    feat: Add function for get product price

--- src/utils/data/dataHandler.js
+++ src/utils/data/dataHandler.js
@@ -11,3 +11,18 @@ export const getProductById = async (id) => {
     }, 1500);
   });
 };
+
+export const getProductPriceById = async (id, amount) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = products.find((product) => product.id == id);
+      if (!result) {
+        reject("Product tidak ditemukan");
+      }
+
+      const actualPrice = result.price - (result.discount / 100) * result.price;
+
+      resolve(actualPrice * amount);
+    }, 1500);
+  });
+};
