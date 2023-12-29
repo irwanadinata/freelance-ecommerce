1a23be04319b9e4dd61ac4df3ce7ef6aed900599
irwanadinata <irwanadinata9@gmail.com>
2023-12-30 04:25:51 +0700

    chore: create button "tampilkan semua"

--- src/pages/dashboard/components/product.jsx
+++ src/pages/dashboard/components/product.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductCard from "@/components/product-card";
+import { Button } from "@/components/ui/button";
 
 const ProductList = ({ products }) => {
   return (
@@ -12,6 +13,15 @@ const ProductList = ({ products }) => {
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      <div className="flex justify-center mt-4">
+        <Button
+          variant="outline"
+          className="lg:w-64 bg-[#F8FAFE] text-[#0F146D] border-2 border-[#0F146D]"
+        >
+          Tampilkan Lebih Banyak
+        </Button>
+      </div>
+      <div class="border-b border-[#C4C4C4] border-solid h-1 lg:w-10/12 md:w-full mx-auto pt-6"></div>
     </div>
   );
 };
