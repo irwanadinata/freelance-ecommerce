03fde691a2e639c71a0019fee78e37ac4b40b8dc
Sam <samilalghiffary@gmail.com>
2023-12-30 21:07:10 +0700

    feat: Create shipping option

--- src/pages/transaction/components/shipping.jsx
+++ src/pages/transaction/components/shipping.jsx
@@ -3,30 +3,75 @@ import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import convertToRupiah from "@/utils/formatter/rupiahConverter";
 
-const ShippingCard = () => {
+const ShippingCard = ({ type, price, expectedArrival1, expectedArrival2, month }) => {
   return (
-    <div className="border-2 flex flex-col gap-y-1 py-2 px-6 border-black rounded-md">
-      <p>{convertToRupiah(16000)}</p>
+    <div className="border-2 flex flex-col gap-y-1 py-2 px-6 w-52 border-black rounded-md">
+      <p>{convertToRupiah(price)}</p>
       <div className="flex justify-between">
-        <p>Standar</p>
-        <Checkbox className="w-6 h-6 border-2 data-[state=checked]:bg-white" />
+        <p>{type}</p>
+        <Checkbox  className="w-6 h-6 border-2 data-[state=checked]:bg-white" />
       </div>
-      21 - 25 Desember
+      {expectedArrival1} - {expectedArrival2} {month}
     </div>
   );
 };
 
-const Shipping = () => {
+const Shipping = ({ store }) => {
+  const getDate = (approx) => {
+    const date = new Date();
+    date.setDate(date.getDate() + approx);
+    return date.getDate();
+  };
+
+  const getMonth = (approx) => {
+    const month = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ];
+
+    const date = new Date();
+    date.setDate(date.getDate() + approx);
+    return month[date.getMonth()];
+  };
+
   return (
     <>
       <div className="p-5 flex gap-y-5 flex-col shadow-md rounded-md bg-white">
         <div className="flex flex-col">
-          <p className="font-medium">Dikirim Oleh: Narvik</p>
+          <p className="font-medium">Dikirim Oleh: {store}</p>
           <p className="font-medium">Pilihan Pengiriman</p>
           <div className="flex justify-between">
-            <ShippingCard />
-            <ShippingCard />
-            <ShippingCard />
+            <ShippingCard
+              type="Standar"
+              price={16000}
+              expectedArrival1={getDate(0)}
+              expectedArrival2={getDate(5)}
+              month={getMonth(5)}
+            />
+            <ShippingCard
+              type="Regular"
+              price={20000}
+              expectedArrival1={getDate(0)}
+              expectedArrival2={getDate(3)}
+              month={getMonth(3)}
+            />
+            <ShippingCard
+              type="Express"
+              price={25000}
+              expectedArrival1={getDate(0)}
+              expectedArrival2={getDate(2)}
+              month={getMonth(2)}
+            />
           </div>
         </div>
 
