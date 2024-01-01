207d9e540f221feb12ece54960e5b90213817860
Sam <samilalghiffary@gmail.com>
2024-01-01 14:56:54 +0700

    feat: Add function for get store name

--- src/utils/data/dataHandler.js
+++ src/utils/data/dataHandler.js
@@ -19,10 +19,21 @@ export const getProductPriceById = async (id, amount) => {
       if (!result) {
         reject("Product tidak ditemukan");
       }
-
       const actualPrice = result.price - (result.discount / 100) * result.price;
-
       resolve(actualPrice * amount);
+    }, 1500);
+  });
+};
+
+export const getStoreNameById = async (id) => {
+  console.log(id);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const result = products.find((product) => product.store.id == id);
+      if (!result) {
+        reject("Terjadi Kesalahan saat mencari nama toko");
+      }
+      resolve(result.store.store_name);
     }, 1500);
   });
 };
