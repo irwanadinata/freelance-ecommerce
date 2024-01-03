b24560e4138441703a2ac314ab269c2ee2f3b937
Sam <samilalghiffary@gmail.com>
2024-01-04 05:41:42 +0700

    fix: change class into className

--- src/pages/dashboard/components/product.jsx
+++ src/pages/dashboard/components/product.jsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ProductCard from "@/components/product-card";
 import { Button } from "@/components/ui/button";
 
 const ProductList = ({ products }) => {
   return (
     <div className="bg-[#FFFAF5] lg:py-4 mb-6">
-      <p className="lg:w-10/12 mx-auto md:w-full font-semibold pb-2">
-        Hanya Untukmu
-      </p>
+      <p className="lg:w-10/12 mx-auto md:w-full font-semibold pb-2">Hanya Untukmu</p>
       <div className="flex flex-wrap justify-center lg:w-10/12 md:w-full mx-auto">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
@@ -21,7 +18,7 @@ const ProductList = ({ products }) => {
           Tampilkan Lebih Banyak
         </Button>
       </div>
-      <div class="border-b border-[#C4C4C4] border-solid h-1 lg:w-10/12 md:w-full mx-auto pt-6"></div>
+      <div className="border-b border-[#C4C4C4] border-solid h-1 lg:w-10/12 md:w-full mx-auto pt-6"></div>
     </div>
   );
 };
