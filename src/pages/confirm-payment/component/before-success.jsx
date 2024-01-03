de8cda1aa995ab55fd51798b031bcd308cf68dce
Sam <samilalghiffary@gmail.com>
2024-01-04 05:43:39 +0700

    feat: Implement dummy payment

--- src/pages/confirm-payment/component/before-success.jsx
+++ src/pages/confirm-payment/component/before-success.jsx
@@ -1,37 +1,95 @@
-import React, { useState, useEffect } from "react";
 import { Copy } from "lucide-react";
-import BNI from "@/assets/payment-method/bni.png";
+import { useState, useEffect } from "react";
+import useCart from "@/utils/store/cartStore";
+import bcaLogo from "@/assets/payment-method/bca.png";
+import bniLogo from "@/assets/payment-method/bni.png";
+import briLogo from "@/assets/payment-method/bri.png";
+import danaLogo from "@/assets/payment-method/dana.png";
 import convertToRupiah from "@/utils/formatter/rupiahConverter";
+import alfamartLogo from "@/assets/payment-method/alfamart.png";
+import indomaretLogo from "@/assets/payment-method/indomaret.png";
+import mastercardLogo from "@/assets/payment-method/mastercard.png";
 
 const BeforeSucces = () => {
-  const [timeRemaining, setTimeRemaining] = useState(getTimeRemaining());
+  const { selectedPaymentMethod, totalPrice } = useCart();
+  const [countdown, setCountdown] = useState({ hours: 24, minutes: 0, seconds: 0 });
+  const [paymentMethod, setPaymentMethod] = useState({ name: "", image: "", code: "" });
 
   useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTimeRemaining(getTimeRemaining());
+    const interval = setInterval(() => {
+      setCountdown((prevCountdown) => {
+        const newCountdown = { ...prevCountdown };
+
+        if (newCountdown.hours === 0 && newCountdown.minutes === 0 && newCountdown.seconds === 0) {
+          clearInterval(interval);
+        } else {
+          if (newCountdown.seconds === 0) {
+            if (newCountdown.minutes === 0) {
+              newCountdown.hours -= 1;
+              newCountdown.minutes = 59;
+              newCountdown.seconds = 59;
+            } else {
+              newCountdown.minutes -= 1;
+              newCountdown.seconds = 59;
+            }
+          } else {
+            newCountdown.seconds -= 1;
+          }
+        }
+
+        return newCountdown;
+      });
     }, 1000);
 
-    return () => clearInterval(intervalId);
+    // Membersihkan interval ketika komponen tidak lagi digunakan
+    return () => clearInterval(interval);
   }, []);
 
-  function getTimeRemaining() {
-    const now = new Date();
-    const deadline = new Date(now);
-    deadline.setHours(24, 0, 0, 0);
+  const getPaymentMethod = (id) => {
+    const timestamp = new Date().getTime();
+    const paymentMethod = [
+      {
+        name: "Dana",
+        image: danaLogo,
+        code: `88810${timestamp}`,
+      },
+      {
+        name: "Kartu Kredit/Debit",
+        image: mastercardLogo,
+      },
+      {
+        name: "BNI Virtual Account",
+        image: bniLogo,
+        code: `009${timestamp}`,
+      },
+      {
+        name: "BCA Virtual Account",
+        image: bcaLogo,
+        code: `80777${timestamp}`,
+      },
+      {
+        name: "BRI Virtual Account",
+        image: briLogo,
+        code: `002${timestamp}`,
+      },
+      {
+        name: "Alfamart",
+        image: alfamartLogo,
+        code: `4774${timestamp}`,
+      },
+      {
+        name: "Indomaret",
+        image: indomaretLogo,
+        code: `1260${timestamp}`,
+      },
+    ];
 
-    const timeDiff = deadline - now;
-    const hours = Math.floor(
-      (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    );
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+    setPaymentMethod(paymentMethod[id]);
+  };
 
-    return {
-      hours: String(hours).padStart(2, "0"),
-      minutes: String(minutes).padStart(2, "0"),
-      seconds: String(seconds).padStart(2, "0"),
-    };
-  }
+  useEffect(() => {
+    getPaymentMethod(selectedPaymentMethod);
+  }, [selectedPaymentMethod]);
 
   return (
     <div className="w-10/12 mx-auto">
@@ -40,36 +98,36 @@ const BeforeSucces = () => {
       </div>
       <div className="text-center w-full">
         <span className="border border-solid border-black p-2 bg-[#FFFFFF]">
-          {timeRemaining.hours}
+          {countdown.hours.toString().padStart(2, "0")}
         </span>
         <span className="mx-2">:</span>
         <span className="border border-solid border-black p-2 bg-[#FFFFFF]">
-          {timeRemaining.minutes}
+          {countdown.minutes.toString().padStart(2, "0")}
         </span>
         <span className="mx-2">:</span>
         <span className="border border-solid border-black p-2 bg-[#FFFFFF]">
-          {timeRemaining.seconds}
+          {countdown.seconds.toString().padStart(2, "0")}
         </span>
         <div className="text-center my-8">
           <p>Batas Akhir Pembayaran</p>
         </div>
       </div>
       <div>
-        <div className="flex my-6">
-          <div>BNI Virtual Account</div>
+        <div className="flex items-center my-6">
+          <div>{paymentMethod.name}</div>
           <div className="ml-auto">
-            <img src={BNI} alt="" className="w-8 h-8" />
+            <img src={paymentMethod.image} className="h-10" />
           </div>
         </div>
-        <div className="flex my-6">
-          <div>6834289746653728</div>
+        <div className="flex items-center my-6">
+          <div>{paymentMethod.code}</div>
           <div className="ml-auto">
             <Copy />
           </div>
         </div>
         <div className="my-4">
           <p className="mb-1 font-semibold">Total Pembayaran</p>
-          <p className="font-semibold">{convertToRupiah(520000)}</p>
+          <p className="font-semibold">{convertToRupiah(totalPrice)}</p>
         </div>
       </div>
     </div>
